2d33bfc3f2d350dc40bd47b6a88af6261a341178
eyobtadele2705 <eyobtadele2705@gmail.com>
2022-09-16 06:22:06 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+*This method: prints lines
+*Returns: nothing
+*/
+
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		putchar('_');
+	}
+	purchar('\n');
+}
+
