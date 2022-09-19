c7cd57a370120db948f2ba896f5fc7fe2493fe61
eyobtadele2705 <eyobtadele2705@gmail.com>
2022-09-19 06:28:15 -0700

    swapping

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,8 +3,8 @@
 /**
 *swap_int - swaps the values of two variables
 *@a : first parameter
-*b : second parameter
-*Returns: nothing
+*@b : second parameter
+*Returns : nothing
 */
 
 void swap_int(int *a, int *b)
