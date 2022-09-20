c30e7cbcc234fafa5a5dc2d96f111961a136da04
eyobtadele2705 <eyobtadele2705@gmail.com>
2022-09-20 06:09:55 -0700

    copy paste

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
-*_strcpy - copy paste
-*@dest: destination
-*@src : source
-*Return : destination
+* _strcpy - Copy paste string
+* @dest: destination
+* @src: source
+*
+* Return: destination
 */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (*(src + i != '\0')
+	while (*(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
 		i++;
 	}
 	*(dest + i) = '\0';
+
 
 	return (dest);
 }
