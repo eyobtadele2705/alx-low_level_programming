84b2cc613b434aa05e8d029c7a594e91fd0fda83
eyobtadele2705 <eyobtadele2705@gmail.com>
2022-09-20 06:04:12 -0700

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,22 +2,22 @@
 #include "main.h"
 
 /**
-* print_array - prints n element of array
-* @a: array
-* @n: number of elements
-* Return: nothing
+*print_array - prints array
+*@a : array
+*@n : number of elements
+*Return : nothing
 */
 
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n ; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (i != n - 1)
-			printf("%d, ", a[i]);
+			printf("%d,", a[i]);
 		else
 			printf("%d", a[i]);
-
 	}
 	putchar(10);
+}
