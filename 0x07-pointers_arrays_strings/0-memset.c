c1232a1e52cf45ffc5338c6db50f65f09d6d3aa6
eyobtadele2705 <eyobtadele2705@gmail.com>
2022-09-26 05:50:24 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@ include "main.h"
 /**
 *_memset - fills a memory block with constant byte
 *@s : address to memory block
-*b : char to be used
+*@b : char to be used
 *@n : number of bytes to be used
 *
 *Return : pointer to the memory block
